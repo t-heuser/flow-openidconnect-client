871844a3f8057519ccc654595c1a241482d8d430
Timon Heuser <timon.heuser@sandstorm.de>
2024-07-25 11:50:28 +0200

    replace session scope for object with putting data directly to the session
    Setting an object to the session via the flow scope leads to bugs as the object is transferred between sessions for whatever reason.

--- Classes/IdentityToken.php
+++ Classes/IdentityToken.php
@@ -11,12 +11,30 @@ use Neos\Flow\Session\SessionManagerInterface;
  *
  * @see https://openid.net/specs/openid-connect-basic-1_0.html#IDToken
  *
- * @Flow\Scope("session")
+ * @Flow\Scope("singleton")
  */
 class IdentityToken extends AbstractToken
 {
-    #[Flow\Inject]
-    protected SessionManagerInterface $sessionManager;
+    private const SESSION_DATA_IDENTIFIER_KEY = 'identity_token_data';
+
+    public function __construct(
+        private readonly SessionManagerInterface $sessionManager
+    ) {
+        $currentSession = $this->sessionManager->getCurrentSession();
+        if ( ! $currentSession->isStarted()) {
+            return;
+        }
+        $dataFromSession = $currentSession->getData(self::SESSION_DATA_IDENTIFIER_KEY);
+        if ( ! is_array($dataFromSession) ||
+            ! array_key_exists('jwt', $dataFromSession) ||
+            ! array_key_exists('oidcServiceName', $dataFromSession) ||
+            ! is_string($dataFromSession['jwt']) ||
+            ! is_string($dataFromSession['oidcServiceName'])) {
+            return;
+        }
+
+        self::setDataFromJwt($dataFromSession['jwt'], $dataFromSession['oidcServiceName']);
+    }
 
     /**
      * Additionally adds a tag to the session to be able to identify the session later on when using the back channel
@@ -32,9 +50,14 @@ class IdentityToken extends AbstractToken
         }
 
         $currentSession = $this->sessionManager->getCurrentSession();
-        // Session is already started at this point, this exception can never really occur.
         $tagPrefix = md5("Flownative-OpenIdConnect-Client-$oidcServiceName");
+        // Session is already started at this point, this exception can never really occur.
         $currentSession->addTag("$tagPrefix-$sessionIdentifier");
+
+        $currentSession->putData(
+            self::SESSION_DATA_IDENTIFIER_KEY,
+            ['jwt' => $this->asJwt(), 'oidcServiceName' => $oidcServiceName]
+        );
     }
 
     public function hasData(): bool
